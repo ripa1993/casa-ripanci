d73f825b3a968a92d9624c839c552013b418efd6
Simone Ripamonti <simone.ripamonti@bitrock.it>
2021-03-13 14:41:13 +0100

    Import

--- WikiTags.py
+++ WikiTags.py
@@ -3,13 +3,13 @@
 __author__ = "Laurent Perrinet"
 __licence__ = 'MIT'
 
-
 import subprocess
-URL = subprocess.check_output(['git', 'remote',  'get-url', 'origin']).decode()
-URL = URL.strip('\n') # stripping last bit given by github
-URL = URL.strip('/') # stripping last bit given by github
-URL = URL.strip('.git') # stripping last bit given by github
-TAG = URL.split('/')[-1] # heuristic to get the project name
+
+URL = subprocess.check_output(['git', 'remote', 'get-url', 'origin']).decode()
+URL = URL.strip('\n')  # stripping last bit given by github
+URL = URL.strip('/')  # stripping last bit given by github
+URL = URL.strip('.git')  # stripping last bit given by github
+TAG = URL.split('/')[-1]  # heuristic to get the project name
 print('TAG =', TAG)
 
 if 'framagit' in URL:
@@ -22,35 +22,36 @@ print('ROOT =', ROOT)
 # print('ROOT =', ROOT)
 SEED = 42
 # SEED = None
-BANDS = ['Talking Heads',
-         # 'Dépèche Mode',
-         'Dead Kennedys',
-         'Neil Young',
-         'Bob Dylan',
-         'Georges Brassens',
-         'Banana Rama',
-         'Bob Marley',
-         'Peter Tosh',
-         'The Clash',
-         'Mulatu Astatke',
-         'Joe Strummer',
-         'Rolling Stones',
-         'Stone Roses',
-         ]
-VERB = True # verbose?
-VERB = False # verbose?
-N_X, N_Y = 5, 8 # columns, rows
-N_X, N_Y = 8, 13 # columns, rows
-N_X, N_Y = 5, 7 # columns, rows
+BANDS = [
+    'Taylor Swift',
+    'Happy Doggo',
+    'Maya Nerone',
+    'Daphne Artu',
+    'Tiger Roll',
+    'Mr Marchesi',
+    'Franco Wow',
+    'Matteo Salvini',
+    'Sergio Mattarella',
+    'Harry Potter',
+    'Hermione Granger',
+    'Gianrico Carofiglio',
+    'Massimo Pericolo',
+    'Daddy Bonaccini',
+    'Silvio Berlusconi',
+]
+VERB = True  # verbose
+N_X, N_Y = 5, 7  # columns, rows
 
 output_dir = './output/'
 from pathlib import Path
+
 Path(output_dir).mkdir(parents=True, exist_ok=True)
 EXT = '.png'
-RESULT = 'grid' # name of the output file
-MARGIN = 0.05 # margin around each axis
-FONTSIZE = 13 # fontsize of the title of each axis
+RESULT = 'grid'  # name of the output file
+MARGIN = 0.05  # margin around each axis
+FONTSIZE = 13  # fontsize of the title of each axis
 import numpy as np
+
 PAPER_RATIO = np.sqrt(2)
 PAGES = 4
 
@@ -65,26 +66,27 @@ if VERB: print('band =', bands_pre, bands_post)
 
 ## shuffling them
 import itertools
+
 bands_list = list(itertools.product(bands_pre, bands_post))
 np.random.seed(SEED)
-ind_bands = np.random.permutation(len(BANDS)**2)
+ind_bands = np.random.permutation(len(BANDS) ** 2)
 if VERB:
     for i_band in ind_bands:
         s1, s2 = bands_list[i_band]
         print('s1 + s2 =', s1 + s2)
 
-
 if VERB:
     url = ROOT + s1 + s2
     print('url =', url)
 
-
 import tempfile
+
 with tempfile.TemporaryDirectory() as tmp:
     if VERB: print("Current temp directory:", tmp)
 
     import os
     import pyqrcode
+
     # https://pythonhosted.org/PyQRCode/rendering.html
     # pip3 install pyqrcode
     # pip3 install pypng
@@ -92,7 +94,7 @@ with tempfile.TemporaryDirectory() as tmp:
     for i_band in ind_bands:
         s1, s2 = bands_list[i_band]
         figname = os.path.join(tmp, s1 + s2 + '.png')
-        if True: #not os.path.isfile(figname):
+        if True:  # not os.path.isfile(figname):
             print('Generating', figname)
             url = ROOT + s1 + s2
             code = pyqrcode.create(url)
@@ -109,15 +111,15 @@ with tempfile.TemporaryDirectory() as tmp:
     import matplotlib.pyplot as plt
 
     # Config:
-    dpi = 100 # dots per inches
-    figsize = 800/dpi # in inches
+    dpi = 100  # dots per inches
+    figsize = 800 / dpi  # in inches
 
     for page in range(PAGES):
         # Create plt plot:
         fig, axs = plt.subplots(N_Y, N_X,
-                                figsize=(N_X*figsize, N_Y*figsize))
+                                figsize=(N_X * figsize, N_Y * figsize))
 
-        for ii, i_band in enumerate(ind_bands[page*(N_X*N_Y):(page+1)*(N_X*N_Y)]):
+        for ii, i_band in enumerate(ind_bands[page * (N_X * N_Y):(page + 1) * (N_X * N_Y)]):
             s1, s2 = bands_list[i_band]
             figname = os.path.join(tmp, s1 + s2 + '.png')
             url = ROOT + s1 + s2
@@ -130,8 +132,9 @@ with tempfile.TemporaryDirectory() as tmp:
 
             ax = axs[y_position, x_position]
             ax.imshow(plt_image, interpolation='nearest', cmap=plt.gray())
-            ax.text(.5, 1-MARGIN, url, horizontalalignment='center',  fontsize=FONTSIZE, transform=ax.transAxes)
-            ax.text(.5, MARGIN/3, s1 + s2, horizontalalignment='center',  fontsize=FONTSIZE*3, transform=ax.transAxes)
+            ax.text(.5, 1 - MARGIN, url, horizontalalignment='center', fontsize=FONTSIZE, transform=ax.transAxes)
+            ax.text(.5, MARGIN / 3, s1 + s2, horizontalalignment='center', fontsize=FONTSIZE * 3,
+                    transform=ax.transAxes)
             # Hide grid lines
             ax.grid(False)
 
@@ -140,9 +143,9 @@ with tempfile.TemporaryDirectory() as tmp:
             ax.set_yticks([])
 
         RESULT_ = TAG + '_' + RESULT
-        if page>0:
+        if page > 0:
             RESULT_ += '_' + str(page)
         RESULT_ += EXT
 
-        plt.subplots_adjust(left=0.0, right=1.0, bottom=0.0, top=1.0, hspace=MARGIN/2, wspace=MARGIN/2)
-        plt.savefig(Path(output_dir) /RESULT_, dpi=dpi)
+        plt.subplots_adjust(left=0.0, right=1.0, bottom=0.0, top=1.0, hspace=MARGIN / 2, wspace=MARGIN / 2)
+        plt.savefig(Path(output_dir) / RESULT_, dpi=dpi)
